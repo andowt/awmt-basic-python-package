ff64c5ebb529252e1c1471bfd2dff6459753b338
Andy West <andy.west@focusrite.com>
2024-12-10 19:46:51 +0000

    Changed main to example

--- AWMT_BasicPythonPackage/example.py
+++ AWMT_BasicPythonPackage/example.py
@@ -2,11 +2,11 @@
 Example main file
 """
 
-def main() -> None:
+def prompt() -> None:
     print('Hello World!')
 
 def add_one(number: int) -> int:
     return number + 1
 
 if __name__ == "__main__":
-    main()
+    prompt()
